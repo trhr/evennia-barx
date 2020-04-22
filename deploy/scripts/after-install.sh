80f5b2f1137edc7ebc937097068b00b9dc5ee0b8
trhr <sage@limitlessinteractive.com>
2020-04-22 18:28:07 -0500

    Deployment changes

--- deploy/scripts/after-install.sh
+++ deploy/scripts/after-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+cp "/home/ec2-user/secrets/barx/secret_settings.py" "/home/ec2-user/barx/server/conf/secret_settings.py"
 export LOGFILE=/home/ec2-user/barx/deploylog
 touch "$LOGFILE"
 echo "AfterInstall completed." >> "$LOGFILE"
