daaed74cd2bbebc3a6ffd6ee32b752d9f66e69b6
trhr <sage@limitlessinteractive.com>
2020-04-22 18:38:38 -0500

    Deployment changes

--- deploy/scripts/validate-service.sh
+++ deploy/scripts/validate-service.sh
@@ -2,7 +2,13 @@
 export LOGFILE=/home/ec2-user/barx/deploylog
 touch "$LOGFILE"
 
-cat /home/ec2-user/barx/server/server.pid
-echo "ValidateService completed." >> $LOGFILE
+if test -f "/home/ec2-user/barx/server/server.pid"; then
+  echo "ValidateService completed." >> $LOGFILE
+  exit 0
+else
+  echo "Could not ValidateService" >> $LOGFILE
+  exit 1
+fi
+
 unset FOLDER
 unset LOGFILE
