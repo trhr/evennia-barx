2f92dec6e8e4ee9b63244e5bf1ecb43ce5f15e2a
trhr <sage@limitlessinteractive.com>
2020-04-22 19:25:32 -0500

    Updated workflow

--- deploy/scripts/after-install.sh
+++ deploy/scripts/after-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp "/home/ec2-user/secrets/barx/secret_settings.py" "/home/ec2-user/barx/server/conf/secret_settings.py"
+pip3 install -r /home/ec2-user/barx/requirements.txt
+
 export LOGFILE=/home/ec2-user/barx/deploylog
 touch "$LOGFILE"
 echo "AfterInstall completed." >> "$LOGFILE"
